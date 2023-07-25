2dc864553eeab46cd7b2d2f2ea9fa190740c8c7d
Erich Schnell <schnellerich@gmail.com>
2023-07-25 17:15:08 -0300

    Agregando Fuentes y Color

---
+++ app/src/main/java/com/example/horsechallenge/Util/Colors.kt
@@ -0,0 +1,25 @@
+package com.example.horsechallenge.util
+
+import androidx.compose.ui.graphics.Color
+
+class color_horse(){
+    val app_color_dark = Color(0xFF1B190F)
+    val app_color_regular = Color(0xFF776E65)
+    val app_color_light = Color(0xFFFAF8EF)
+
+    val black_cell = Color(0xFF423327)
+    val black_cell_premium = Color(0xFF185303)
+
+    val white_cell = Color(0xFFEFCF9B)
+    val white_cell_premium = Color(0xFFeaf7e0)
+
+    val contrast_data = Color(0xFFa40d0d)
+    val contrast_cell_premium = Color(0xFF17711b)
+
+    val selected_cell = Color(0xFF780533)
+    val previus_cell = Color(0xFFffa500)
+    val enable_cell = Color(0xFF1775a4)
+    val message_bg = Color(0xcdfaf8ef)
+
+}
+
--- app/src/main/java/com/example/horsechallenge/horseGame/ui/HorseGameScreen.kt
+++ app/src/main/java/com/example/horsechallenge/horseGame/ui/HorseGameScreen.kt
@@ -1,94 +1,108 @@
 package com.example.horsechallenge.horseGame.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.clipScrollableContainer
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.horsechallenge.util.*
+
+val color = color_horse()
+val fuente = fontAmaranth()
 
 @Preview
 @Composable
 fun HorseGameScreen() {//horseGameViewModel: HorseGameViewModel
-    Box(modifier = Modifier.fillMaxSize()){
-        Encabezado()
-    }
-}
-
-
-@Composable
-fun Encabezado() {
     Column(
-        Modifier
-            .fillMaxWidth()
-            .background(Color.White)
-            .padding(top = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            "Horse Chanllege",
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            color = Color.Gray,
-            fontWeight = FontWeight.ExtraBold,
-            fontSize = 20.sp,
-            fontStyle = FontStyle.Italic
-        )
-        CardInformativa(
-            modifier = Modifier.fillMaxWidth().padding(4.dp),
-            title = "Level",
-            CardColor = Color.Black,
-            dato = "1"
-        )
-        Row{
-            CardInformativa(
-                modifier = Modifier.padding(4.dp),
-                title = "Moves",
-                CardColor = Color.Gray,
-                dato = "63"
-            )
-            CardInformativa(
-                modifier = Modifier.padding(4.dp),
-                title = "Time",
-                CardColor = Color.Gray,
-                dato = "00:12"
-            )
-            CardInformativa(
-                modifier = Modifier.padding(4.dp),
-                title = "Lives",
-                CardColor = Color.Red,
-                dato = "1"
-            )
-            CardInformativa(
-                modifier = Modifier.padding(4.dp),
-                title = "Options",
-                CardColor = Color.Gray,
-                dato = "6"
-            )
-        }
-
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color.app_color_light)
+    ){
+        AdvertenciaFree()
+        Body(Modifier.weight(1f))
+        Publicidad()
     }
 }
 
+
 @Composable
-fun CardInformativa(modifier: Modifier, title: String, CardColor: Color, dato: String) {
+fun AdvertenciaFree() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color.contrast_data)
+            .padding(vertical = 8.dp)
+            .clickable {
+                //TODO
+            },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Tap here to remove ads, get more levels and unlimited lives",
+            color = color.app_color_light,
+            fontFamily = fuente.amaranthFamily,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+@Composable
+fun Body(modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .clipScrollableContainer(orientation = Orientation.Vertical),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        Text(
+            modifier = Modifier.padding(top = 22.dp),
+            text = "Horse Chanllege",
+            color = color.app_color_regular,
+            fontFamily = fuente.amaranthFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 26.sp
+        )
+        CardInformativa(modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .size(80.dp),
+            title = "Level", CardColor = Color.Black, dato = "1"
+        )
+        Row(Modifier.padding(horizontal = 16.dp)){
+            CardInformativa(modifier = Modifier.weight(1f), title = "Moves", CardColor = color.app_color_regular, dato = "63")
+            CardInformativa(modifier = Modifier.weight(1f), title = "Time", CardColor = color.app_color_regular, dato = "00:12")
+            CardInformativa(modifier = Modifier.weight(1f), title = "Lives", CardColor = color.contrast_data, dato = "1")
+            CardInformativa(modifier = Modifier.weight(1f), title = "Options", CardColor = color.app_color_regular, dato = "6")
+        }
+        Tablero()
+    }
+
+}
+@Composable
+fun CardInformativa(modifier: Modifier = Modifier,title: String, CardColor: Color, dato: String) {
     Card(
-        modifier = modifier,
+        modifier = modifier.padding(4.dp).size(50.dp),
         colors = CardDefaults.cardColors(containerColor = CardColor)
     ) {
+        Spacer(modifier = Modifier.weight(1f))
         Text(
             text = title,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
+            modifier = Modifier.padding(bottom = 4.dp).align(Alignment.CenterHorizontally),
             color = Color.White
         )
         Text(
@@ -96,15 +110,30 @@ fun CardInformativa(modifier: Modifier, title: String, CardColor: Color, dato: S
             modifier = Modifier.align(Alignment.CenterHorizontally),
             color = Color.White
         )
+        Spacer(modifier = Modifier.weight(1f))
     }
 }
 
 @Composable
 fun Tablero() {
-    TODO("Not yet implemented")
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(400.dp)
+            .background(Color.Gray)
+    ){
+
+    }
 }
 
 @Composable
 fun Publicidad() {
-    TODO("Not yet implemented")
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(100.dp)
+            .background(Color.Green)
+    ){
+
+    }
 }
